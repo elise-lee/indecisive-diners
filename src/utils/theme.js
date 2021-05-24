151b606f77da2c771083cd57801c14281d033be4
Elise Lee <eliselee2021@u.northwestern.edu>
2021-05-24 14:21:26 -0500

    CSS tweaks

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -10,8 +10,8 @@ const theme = {
           paddingX: 12,
           paddingY: 8,
           borderRadius: 5,
-          backgroundColor: '#4e6e91',
-          opacity: '0.7',
+          backgroundColor: '#f26346',
+          opacity: '0.8',
           transition: '0.5s',
           _hover: {
             opacity: '0.9',
